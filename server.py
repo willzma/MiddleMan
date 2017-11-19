0b3d1818bfb9592879f0af5edcf993d7afae0237
Chaitya Shah <chaityabshah@verizon.net>
2017-11-19 16:46:00 -0500

    init

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('data')
+def test_message(message):
+    #emit('my response', {'data': 'got it!'})
+    print(data)
+
+if __name__ == '__main__':
+    socketio.run(app, port=38300)
